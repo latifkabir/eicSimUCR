582958746bf537e009689cb15a58031c8acac7b6
Latif Kabir <siplukabir@gmail.com>
2020-02-12 15:15:33 -0500

    Added BasicDIS example

---
+++ src/BasicDIS.h
@@ -0,0 +1,7 @@
+// Filename: BasicDIS.h
+// Description: 
+// Author: Latif Kabir < kabir@bnl.gov >
+// Created: Wed Feb 12 14:53:13 2020 (-0500)
+// URL: jlab.org/~latif
+
+int BasicDIS(int nEvents = 1000);
--- src/LinkDef.h
+++ src/LinkDef.h
@@ -5,6 +5,7 @@
 #pragma link off all functions;
 
 #pragma link C++ function help;                          // Print all functions implemented
+#pragma link C++ function BasicDIS;                      // Generate basic ep DIS events using Pythia8 
 #pragma link C++ function ElecJetKin;                    // Generate e and Jet Kinematics using FastJet and Pythia8 
 #pragma link C++ function MakeHepMc;                     // Generate HepMC file from Pythia 8   
 #pragma link C++ function BuildBeAST;                    // Build BeAST detector to be used for smearing
